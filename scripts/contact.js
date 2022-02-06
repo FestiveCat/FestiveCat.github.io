e80c2754acca328d8737e1fbb76951ed904eca74
FestiveCat <31hanyang@gmail.com>
2022-02-07 01:29:58 +0800

    fix contact me

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,5 +1,7 @@
 window.onload = function() {
     document.getElementById('form').addEventListener('submit', function(event) {
+        var valid = validateForm();
+        if (!valid) return;
         event.preventDefault();
         // these IDs from the previous steps
         emailjs.send("service_ul78njc","template_u7xraid",{
@@ -9,9 +11,36 @@ window.onload = function() {
             message: document.forms["form"]["form-contents"].value,
         })
             .then(function() {
+                alert("Email sent!");
                 console.log('SUCCESS!');
             }, function(error) {
+                alert("Something went wrong... please email me manually!");
                 console.log('FAILED...', error);
             });
     });
+}
+
+function validateForm() {
+    var subject = document.forms["form"]["form-subject"].value;
+    if (subject == "") {
+        alert("Subject cannot be empty!");
+        return false;
+    }
+    var senderName = document.forms["form"]["form-name"].value;
+    if (senderName == "") {
+        alert("Please enter a name!");
+        return false;
+    }
+    var content = document.forms["form"]["form-contents"].value;
+    if (content == "") {
+        alert("Please enter a name!");
+        return false;
+    }
+    var email = document.forms["form"]["form-email"].value;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!email.match(mailformat)) {
+        alert("Please enter a valid email!");
+        return false;
+    }
+    return true;
 }
