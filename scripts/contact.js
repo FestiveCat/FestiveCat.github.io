7934cbf19b9130aa59275e9e5400cba6c933a4bb
FestiveCat <31hanyang@gmail.com>
2022-02-07 01:03:52 +0800

    attempt 3 for contact me

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,12 +1,21 @@
-//6LcSjF8eAAAAAPB8GYb6okGmJXdxoi9BGRAYzv0p
 function onSubmit(token) {
     document.getElementById("demo-form").submit();
 }
-function submitForm() {
-    emailjs.send("service_ul78njc","template_u7xraid",{
-        subject: document.forms["form"]["form-subject"],
-        from_name: document.forms["form"]["form-name"],
-        from_email: document.forms["form"]["form-email"],
-        message: document.forms["form"]["form-contents"],
+
+window.onload = function() {
+    document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.send("service_ul78njc","template_u7xraid",{
+            subject: document.forms["form"]["form-subject"].value,
+            from_name: document.forms["form"]["form-name"].value,
+            from_email: document.forms["form"]["form-email"].value,
+            message: document.forms["form"]["form-contents"].value,
+        })
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
     });
 }
